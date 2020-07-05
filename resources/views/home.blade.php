12ce68bcfbcecce383d4aeccd988bf3aa8227235
Biskilicious <rbdundas@gmail.com>
2020-07-05 10:09:12 -0700

    Added Google Analytics to header template.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,27 +2,15 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @can('isAdmin')
-                        You're an admin!
-                    @elsecan('isUser')
-                        You're a User!
-                    @else
-                        You're a Guest!
-                    @endcan
-                </div>
-            </div>
+    <div class="columns">
+        <div class="column">
+            @can('isAdmin')
+                <a href="{{ route('post.new') }}">Create a new post</a>
+            @elsecan('isUser')
+                You're a User!
+            @else
+                You're a Guest!
+            @endcan
         </div>
     </div>
 </div>
